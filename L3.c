9ccae939de11d131fbe44c45a56b7380fc0648bc
Rockie1601 <143343634+Rockie1601@users.noreply.github.com>
2023-09-10 22:40:42 -0500

    Update L3.c

--- L3.c
+++ L3.c
@@ -2,8 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-// Function to read strings from the user
-char** readStrings(int m) {
+char** readStr(int m) {
     char** y = (char**)malloc(m * sizeof(char*));
     if (y == NULL) {
         perror("Memory allocation failed");
@@ -11,12 +10,12 @@ char** readStrings(int m) {
     }
 int p=0;
         do{
-        printf("Enter string %d: ", p + 1);
+        printf("Enter str %d: ", p + 1);
         char e[100]; //
         scanf("%s", e);
         y[p] = (char*)malloc((strlen(e) + 1) * sizeof(char));
         if (y[p] == NULL) {
-            perror("Memory allocation failed");
+            perror("Memory alloc failed");
             exit(1);
         }
         strcpy(y[p], e);
@@ -27,8 +26,7 @@ int p=0;
     return y;
 }
 
-// Function to perform insertion sort on an array of strings
-void insertionSort(char **uu, int n2) {
+void insSort(char **uu, int n2) {
     for (int y = 1; y < n2; y++) {
         char *val = uu[y];
         int k = y - 1;
@@ -40,8 +38,7 @@ void insertionSort(char **uu, int n2) {
     }
 }
 
-// Function to display sorted strings
-void displayStrings(char** str, int n2) {
+void displayStr(char** str, int n2) {
     printf("Sorted :\n");
     int e=0;
     while(e<n2){
@@ -50,8 +47,7 @@ void displayStrings(char** str, int n2) {
     }
 }
 
-// Function to free memory
-void freeStrings(char** strs, int f) {
+void freeStr(char** strs, int f) {
     for (int t = 0; t < f; t++) {
         free(strs[t]);
     }
@@ -61,21 +57,16 @@ void freeStrings(char** strs, int f) {
 int main() {
     int n;
 
-    // Enter the number of strings
-    printf("Enter the number of strings: ");
+    printf("Enter the number of str: ");
     scanf("%d", &n);
 
-    // Read the strings
-    char** strings = readStrings(n);
+    char** strings = readStr(n);
 
-    // Sort the strings
-    insertionSort(strings, n);
+    insSort(strings, n);
 
-    // Display the sorted strings
-    displayStrings(strings, n);
+    displayStr(strings, n);
 
-    // Free memory
-    freeStrings(strings, n);
+    freeStr(strings, n);
 
     return 0;
 }
